3ee1fe430b3595becd2ef9bf676544acedf33e20
HOME-VM-TOSCHOOL <mat.dok123@gmail.com>
2023-12-29 05:31:45 +0100

    timestamp_format

--- theEye.py
+++ theEye.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import json
+import pytz
 
 def read_json_file(file_path):
     with open(file_path, 'r') as file:
@@ -16,22 +18,25 @@ def transcode_latin1_to_utf8(conversation):
         message.update({'content': transcoded_message})
     return conversation
 
-def print_json_structure(data, indent=2):
-    def explore_structure(obj, current_indent):
-        if isinstance(obj, dict):
-            for key, value in obj.items():
-                print(" " * current_indent + f'"{key}":')
-                explore_structure(value, current_indent + indent)
-        elif isinstance(obj, list):
-            for item in obj:
-                explore_structure(item, current_indent + indent)
-
-    explore_structure(data, 0)
+def change_timestamp_format(conversation):
+    for i in range(len(conversation.get('messages'))):
+        message = conversation.get('messages')[i]
+        timestamp_milliseconds = message.get('timestamp_ms')
+        timestamp_seconds = timestamp_milliseconds / 1_000
+        dt_object = datetime.utcfromtimestamp(timestamp_seconds)
+        
+        # Apply timezone offset
+        timezone_offset = -2  # Replace with your desired timezone offset
+        dt_object_with_tz = dt_object.replace(tzinfo=pytz.utc).astimezone(pytz.timezone(f'Etc/GMT{timezone_offset:+}'))
+        
+        iso8601_date = dt_object_with_tz.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + dt_object_with_tz.strftime('%Z:00')
+        message.update({'timestamp_ms': iso8601_date})
+    return conversation
 
 out = read_json_file('message_1.json')
 
-print(print_json_structure(out))
-
 out = transcode_latin1_to_utf8(out)
+
+out = change_timestamp_format(out)
     
-# print(out.get('messages'))
+print(out.get('messages')[0])
